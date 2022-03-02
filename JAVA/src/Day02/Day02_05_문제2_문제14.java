30a1908baf2d8ac44a2a4567bb1708755c71f42a
504 <504@DESKTOP-2M11V1C>
2022-03-02 20:37:37 +0900

    Day 02

--- JAVA/src/Day02/Day02_05_문제2_문제14.java
+++ JAVA/src/Day02/Day02_05_문제2_문제14.java
@@ -242,18 +242,7 @@ public class Day02_05_문제2_문제14 { // c.s
 //				System.out.println(" 더 큰 수는 :" + 큰수);
 //				
 				
-			/*	// *조건 연산자 예제
-				int 점수 = 65;
-				char 등급 = 점수 > 90 ? 'A' : 'B';
-							// 조건식  ? 참(true) : 거짓(false)
-							// 점수 변수가 90 초과 하면 A 대입 아니면 B 대입
-				System.out.println(" 등급 : " + 등급);
-				
-				char 등급2 = 점수 > 90 ? 'A' : 점수 > 80 ? 'B' : 'C';
-							// 조건식  ? 참 : 거짓[ 조건식 ? 참 : 거짓]
-							// 점수 변수가 90점을 초과하면 A 대입 아니면 점수 변수가 80 초과하면 B 대입 아니면 C
-				System.out.println(" 등급 : " + 등급2);
-				*/
+			
 				
 				System.out.println("문제 16) 정수입력1"); int 정수1=scanner.nextInt();
 				System.out.println("문제 16) 정수입력2"); int 정수2=scanner.nextInt();
@@ -261,19 +250,159 @@ public class Day02_05_문제2_문제14 { // c.s
 				
 //				int 큰수 = 문제6_1>문제6_2 ? 문제6_1: 문제6_2;
 				//A B C
-				// A>B && A>C && 
-				int 큰값찾기 =   ((정수1 > 정수2  )&&(정수1>정수3)? 정수1 : 정수2  ;
-				int 중간값찾기 =  (정수2 > 정수3  )? 정수2  : 정수3  ;
-				int 작은값찾기 =  (정수2 < 정수3  )? 정수3  : 정수2  ;
+				// Max value
+				//1-1. A>=B && A>=C 
+				//   A가 B 보다 크거나 같고 A가 C 보다 크거나 같다  
+				//1-2. B>=A&&B>=C  
+				//   B가 A보다 크거나 같고 C보다 크거나 같다 
+				//  (A>=B && A>=C) ? A : (B>=A && B>=C) ? B : C
+				     // A가 B 보다 크거나 같고 A가 C 보다 크거나 같으면 max value에 A 입력 
+						// 1-1 비 만족 시 B가 A 보다 크거나 같고 C 보다 크거나 같으면 max value 에 B 입력
 				
+				// min value -> max value 와 반대
+				//2-1. A<=B && A<=C 
+				//   A가 B 보다 작거나 같고 A가 C 보다 작거나 같다   
+				//2-2. B<=A&&B<=C  
+				//   B가 A보다 작거나 같고 C보다 작거나 같다 
+				//  (A<=B && A<=C) ? A : (B<=A && B<=C) ? B : C
+				     // A가 B 보다 작거나 같고 A가 C 보다 작거나 같으면 min value에 A 입력 
+						// 2-1 비 만족 시 B가 A 보다 작거나 같고 C 보다 작거나 같으면 max value 에 B 입력
 				
+				// mid value
+				// A 가 중간 값일 경우   B<A<C    & C<A<B
+				//                  A>=B&&A<=C : B<A<C
+				//					(정수1>= 정수2 && 정수1 <= 정수3)
+				//					 	A>=B&&A<=C ? A -> B<A<C 조건이 맞을 경우 중간값 A
+				//	
+				//                  A>=C&&A<=B : C<A<B
+//				 					(정수1>= 정수3 && 정수1 <= 정수2)	
+				//						A>=C&&A<=B ? A : C<A<B 조건이 맞을 경우 중간값 A
+ 				// B 가 중간 값일 경우   A<B<C & C<B<A
+				//					B>=A && B<=C : A<B<C
+				//					(정수2>=정수1 && 정수2<=정수3)
+				//					B>=C && B<=A : C<B<A
+				//					(정수2>=정수3 && 정수2<=정수1)
+				// C 가 중간 값일 경우   A<C<B & B<C<A
+				//                  C>=A && C<=B : A<C<B 
+				//					(정수3>=정수1 && 정수3<=정수2)
+				//					C<=B && C>=A : B<C<A
+				//					(정수3>=정수2 && 정수3<=정수1)
+				// A>=B&&A<=C ? A : A>=C&&A<=B ? A : B>=A && B<=C ? B : B>=C&& B<=A? B : C>=A&& C<=B ? 
+				//				C : C<=B && C>=A ? C
+				// 
+				int 큰값 = 정수1 >=정수2 && 정수1>=정수3 ? 정수1 : 정수2>=정수1&&정수2>=정수3? 정수2 : 정수3; 
+				int 작은값 = 정수1 <=정수2 && 정수1<=정수3 ? 정수1 : 정수2<=정수1&&정수2<=정수3? 정수2 : 정수3;
+				int 중간값 = (정수1>= 정수2 && 정수1 <= 정수3)? 정수1 : (정수1>= 정수3 && 정수1 <= 정수2)? 정수1 :
+						   (정수2>=정수1 && 정수2<=정수3) ? 정수2 : (정수2>=정수3 && 정수2<=정수1) ? 정수2: 
+						   (정수3>=정수1 && 정수3<=정수2)? 정수3 :	(정수3>=정수2 && 정수3<=정수1)? 정수3 : 정수1; 
+						
+						
+						
+				System.out.printf("오름차순 \nLow value=%d\nMid value=%d\nMax value=%d",작은값,중간값,큰값);
 				
-				/*
-				 * int 큰수찾기1 = 정수1 > 정수2 ? 정수1 : 정수2; int 큰수찾기2 = 정수2 > 정수3 ? 정수2 : 정수3; int
-				 * 큰수찾기3 = 정수1 > 정수3 ? 정수1 : 정수3;
-				 */		
-				
+			
 				//문제17 : 4개의 정수를 입력받아 내림차순으로 정렬 
+				// Max & Low 완 Mid1&2 작성
+				System.out.println("문제 17) 정수입력1"); int A=scanner.nextInt();
+				System.out.println("문제 17) 정수입력2"); int B=scanner.nextInt();
+				System.out.println("문제 17) 정수입력3"); int C=scanner.nextInt();
+				System.out.println("문제 17) 정수입력4"); int D=scanner.nextInt();
+				
+				System.out.printf("\n%d,%d,%d,%d",A,B,C,D);
+				
+				// A B C D 
+				// Max A = 1)D<C<B<A && 2) C<D<B<A && 3) D<B<C<A  && 4)C<B<D<A  && 5) B<C<D<A  && 6)B<D<C<A
+				//         1)A>=B&& B>=C && C>=D
+				//                      2) A>=B && B>=D && D>= C
+				//									  3) A>=C&&C>=B&&>B>=D
+				//													 4) A>=D&&D>=B&&B>=C
+				//																    5) A>=D&&D>=C&&C>=B
+				//																			6) A>=C&&C>=D&&D>=B
+				// Max B = D<C<A<B && C<D<A<B && D<A<C<B  && C<A<D<B  && A<C<D<B  && A<D<C<B
+				//         B>=A&&A>=C&&C>=D 
+				//					B>=A&&A>=D&&D>=C
+				//  							B>=C&&C>=A&&A>=D
+				//										B>=D&&D>=A&&A>=C
+				//												B>=D&&D>=C&&C>=A
+				// 														B>=C&&C>=D&&D>=A
+				
+				// Max C = D<A<B<C && A<D<B<C && D<B<A<C  && A<B<D<C  && B<A<D<C  && B<D<A<C	
+//						   C>=B&&B>=A&&A>=D
+				//					C>=B&&B>=D&&D>=A
+//				//								C>=A&&A>=B&&B>=D
+//														C>=D&&D>=B&&B>=A
+//																C>=D&&D>=A&&A>=B	
+//																		C>=A&&A>=B&&B>=D
+																		
+				// Max D = A<C<B<D && C<A<B<D && A<B<C<D  && C<B<A<D  && B<C<A<D  && B<A<C<D	
+//						   D>=B&&B>=C&&C>=A
+//						   			D>=B&&B>=A&&A>=C
+//						   					D>=C&&C>=B&&B>=A
+//						   							D>=A&&A>=B&&B>=C
+//						   									D>=A&&A>=C&&C>=B
+//						   											D>=C&&C>=A&&A>=B
+//				
+//				
+				int 큰값2 = A>=B && B>=C && C>=D ? A: A>=B && B>=D && D>=C ? A : 
+						   A>=C&&C>=B&&B>=D ? A : A>=D&&D>=C&&C>=B? A : A>=C&&C>=D&&D>=B? A:
+							
+						   B>=A&&A>=C&&C>=D? B : B>=A&&A>=D&&D>=C ? B :	B>=C&&C>=A&&A>=D? B:
+						   B>=D&&D>=A&&A>=C? B : B>=D&&D>=C&&C>=A ? B : B>=C&&C>=D&&D>=A? B:
+							   
+						   C>=B&&B>=A&&A>=D ? C	: C>=B&&B>=D&&D>=A ? C : C>=A&&A>=B&&B>=D? C :
+						   C>=D&&D>=B&&B>=A? C: C>=D&&D>=A&&A>=B?C: C>=A&&A>=B&&B>=D?C:
+							
+						   D>=B&&B>=C&&C>=A ? D : D>=B&&B>=A&&A>=C? D : D>=C&&C>=B&&B>=A? D:
+						   D>=A&&A>=B&&B>=C ? D : D>=A&&A>=C&&C>=B ? D : D>=C&&C>=A&&A>=B ? D : A;	   
+					    
+
+			    int 작은값2 = A<=B && B<=C && C<=D ? A: A<=B && B<=D && D<=C ? A : 
+						   A<=C&&C<=B&&B<=D ? A : A<=D&&D<=C&&C<=B? A : A<=C&&C<=D&&D<=B? A:
+							
+						   B<=A&&A<=C&&C<=D? B : B<=A&&A<=D&&D<=C ? B :	B<=C&&C<=A&&A<=D? B:
+						   B<=D&&D<=A&&A<=C? B : B<=D&&D<=C&&C<=A ? B : B<=C&&C<=D&&D<=A? B:
+							   
+						   C<=B&&B<=A&&A<=D ? C	: C<=B&&B<=D&&D<=A ? C : C<=A&&A<=B&&B<=D? C :
+						   C<=D&&D<=B&&B<=A? C: C<=D&&D<=A&&A<=B?C: C<=A&&A<=B&&B<=D?C:
+							
+						   D<=B&&B<=C&&C<=A ? D : D<=B&&B<=A&&A<=C? D : D<=C&&C<=B&&B<=A? D:
+						   D<=A&&A<=B&&B<=C ? D : D<=A&&A<=C&&C<=B ? D : D<=C&&C<=A&&A<=B ? D : A;	   
+				
+			    // 중간값 잠시 작은값 복붙시킴 
+				int 두번째값 =	A<=B && B<=C && C<=D ? A: A<=B && B<=D && D<=C ? A : 
+					   A<=C&&C<=B&&B<=D ? A : A<=D&&D<=C&&C<=B? A : A<=C&&C<=D&&D<=B? A:
+							
+					   B<=A&&A<=C&&C<=D? B : B<=A&&A<=D&&D<=C ? B :	B<=C&&C<=A&&A<=D? B:
+					   B<=D&&D<=A&&A<=C? B : B<=D&&D<=C&&C<=A ? B : B<=C&&C<=D&&D<=A? B:
+						   
+					   C<=B&&B<=A&&A<=D ? C	: C<=B&&B<=D&&D<=A ? C : C<=A&&A<=B&&B<=D? C :
+					   C<=D&&D<=B&&B<=A? C: C<=D&&D<=A&&A<=B?C: C<=A&&A<=B&&B<=D?C:
+						
+					   D<=B&&B<=C&&C<=A ? D : D<=B&&B<=A&&A<=C? D : D<=C&&C<=B&&B<=A? D:
+					   D<=A&&A<=B&&B<=C ? D : D<=A&&A<=C&&C<=B ? D : D<=C&&C<=A&&A<=B ? D : A;	
+				
+				// 중간값 잠시 작은값 복붙시킴 
+				int 세번째값 =	A<=B && B<=C && C<=D ? A: A<=B && B<=D && D<=C ? A : 
+					   A<=C&&C<=B&&B<=D ? A : A<=D&&D<=C&&C<=B? A : A<=C&&C<=D&&D<=B? A:
+							
+					   B<=A&&A<=C&&C<=D? B : B<=A&&A<=D&&D<=C ? B :	B<=C&&C<=A&&A<=D? B:
+					   B<=D&&D<=A&&A<=C? B : B<=D&&D<=C&&C<=A ? B : B<=C&&C<=D&&D<=A? B:
+						   
+					   C<=B&&B<=A&&A<=D ? C	: C<=B&&B<=D&&D<=A ? C : C<=A&&A<=B&&B<=D? C :
+					   C<=D&&D<=B&&B<=A? C: C<=D&&D<=A&&A<=B?C: C<=A&&A<=B&&B<=D?C:
+						
+					   D<=B&&B<=C&&C<=A ? D : D<=B&&B<=A&&A<=C? D : D<=C&&C<=B&&B<=A? D:
+					   D<=A&&A<=B&&B<=C ? D : D<=A&&A<=C&&C<=B ? D : D<=C&&C<=A&&A<=B ? D : A;	
+				
+				
+				System.out.printf("\n%d\n,%d\n,%d\n,%d",큰값2,두번째값,세번째값,작은값2);
+				
+				// Min A = A<B<C<D && A<B<D<C && A<C<B<D  && A<D<B<C  && A<C<D<B  && A<D<C<B
+				// Min B = B<A<C<D && B<A<D<C && B<C<A<D  && B<D<A<C  && B<C<D<A  && B<D<C<A
+				// Min C = C<A<B<D && C<A<D<B && C<B<A<D  && C<D<A<B  && C<B<D<A  && C<D<B<A				
+				// Min D = D<A<B<C && D<A<C<B && D<B<A<C  && D<C<A<B  && D<B<C<A  && D<C<B<A
+				
+				
 				
 				// p. 102 확인문제
 
